ba9e7b1f4487e74b9b2ea4780a6c04a5de9e7ce6
jpemberthy <jpemberthy@gmail.com>
2008-12-10 16:34:39 -0500

    Graphic Initialize

--- lib/ezgraphix.rb
+++ lib/ezgraphix.rb
@@ -1,2 +1,25 @@
-module Ezgraphix
+unless defined? Ezgraphix
+  
+  module Ezgraphix
+    
+    #Graphic sounds good but maybe a lot of ppl will be already using that name for a class
+    #will change it!
+    class Graphic
+      
+      attr_accessor :c_type, :w, :h, :id_name
+      
+      def initialize(options={})
+        options.merge!(self.defaults)
+        @c_type = options[:c_type]
+        @w = options[:w]
+        @h = options[:y]
+        @id_name = options[:id_name]
+      end
+      
+      #c_type chart type, :w with, :h heigth
+      def defaults
+        {:c_type => 'bar3d', :w => 300, :h => 300, :id_name => 'basic_graph'}  
+      end
+      
+  end
 end
