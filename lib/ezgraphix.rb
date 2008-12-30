1a5545919dd769f8f1011ab0c9939073033c1d8a
jpemberthy <jpemberthy@gmail.com>
2008-12-30 13:27:58 -0500

    Adding render_options attribute for Graphic, and rand_color method.

--- lib/ezgraphix.rb
+++ lib/ezgraphix.rb
@@ -6,7 +6,7 @@ unless defined? Ezgraphix
     #will change it!
     class Graphic
       
-      attr_accessor :c_type, :w, :h, :id_name
+      attr_accessor :c_type, :w, :h, :id_name, :render_options
       
       COLORS = ['AFD8F8', 'F6BD0F', '8BBA00', 'FF8E46', '008E8E',
                 'D64646', '8E468E', '588526', 'B3A00', 'B2FF66',
@@ -14,7 +14,7 @@ unless defined? Ezgraphix
                 '66FF33', '000000', 'FFFF00', '669966', 'FF3300']
       
       def initialize(options={})
-        options.merge!(self.defaults)
+        @render_options = options.merge!(self.defaults)
         @c_type = options[:c_type]
         @w = options[:w]
         @h = options[:y]
@@ -24,6 +24,10 @@ unless defined? Ezgraphix
       #c_type chart type, :w width, :h heigth
       def defaults
         {:c_type => 'bar3d', :w => 300, :h => 300, :id_name => 'basic_graph'}  
+      end
+      
+      def rand_color
+        return COLORS[rand(Graph::COLORS.size - 1)]
       end
       
   end
