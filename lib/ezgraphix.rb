f3dc560e1636bad4777c4efaf8c10974ebef6783
jpemberthy <jpemberthy@gmail.com>
2008-12-30 12:50:26 -0500

    Adding COLORS

--- lib/ezgraphix.rb
+++ lib/ezgraphix.rb
@@ -8,6 +8,11 @@ unless defined? Ezgraphix
       
       attr_accessor :c_type, :w, :h, :id_name
       
+      COLORS = ['AFD8F8', 'F6BD0F', '8BBA00', 'FF8E46', '008E8E',
+                'D64646', '8E468E', '588526', 'B3A00', 'B2FF66',
+                'F984A1', 'A66EDD', 'B2FF66', '3300CC', '000033',
+                '66FF33', '000000', 'FFFF00', '669966', 'FF3300']
+      
       def initialize(options={})
         options.merge!(self.defaults)
         @c_type = options[:c_type]
